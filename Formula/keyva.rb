1a0ba24109c91ee9a03d07d64856198435415578
Frank Sera <78000380+seracreativo@users.noreply.github.com>
2025-12-19 01:32:13 +0100

    v1.3.6: Fix database lock - CLI works while app is open

--- Formula/keyva.rb
+++ Formula/keyva.rb
@@ -1,16 +1,16 @@
 class Keyva < Formula
   desc "Environment variables manager CLI - works with Keyva iOS/macOS app"
   homepage "https://github.com/seracreativo/keyva-cli"
-  version "1.3.5"
+  version "1.3.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.5/keyva-1.3.5-arm64.tar.gz"
-      sha256 "3fcab6d913397336d2ffddedad6319c0a23022c5edb0cef6fc35f1c4399f7ddc"
+      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.6/keyva-1.3.6-arm64.tar.gz"
+      sha256 "a376e1a5cf8b3133396e6f5fea0ab68395ceb69982c2d31ccc8ad000dc1205dc"
     else
       # Intel Mac - will need to add this binary later
-      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.5/keyva-1.3.5-x86_64.tar.gz"
+      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.6/keyva-1.3.6-x86_64.tar.gz"
       sha256 "PLACEHOLDER_X86_SHA256"
     end
   end
