ecc0a8cb80196cb38db57532e95ef4a2dafa2a7f
Frank Sera <78000380+seracreativo@users.noreply.github.com>
2025-12-19 01:54:31 +0100

    v1.3.7: Fix menu display + add self-update

--- Formula/keyva.rb
+++ Formula/keyva.rb
@@ -1,16 +1,16 @@
 class Keyva < Formula
   desc "Environment variables manager CLI - works with Keyva iOS/macOS app"
   homepage "https://github.com/seracreativo/keyva-cli"
-  version "1.3.6"
+  version "1.3.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.6/keyva-1.3.6-arm64.tar.gz"
-      sha256 "5a0aa1795d36b1cb8f2066c54b02616a68fae7b7c39064c47c8125706f11777c"
+      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.7/keyva-1.3.7-arm64.tar.gz"
+      sha256 "b0e6400b42f95183770b1d1ef01adf18bfd0b1e411275a5123f1a0572847e07a"
     else
       # Intel Mac - will need to add this binary later
-      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.6/keyva-1.3.6-x86_64.tar.gz"
+      url "https://github.com/seracreativo/keyva-cli/releases/download/v1.3.7/keyva-1.3.7-x86_64.tar.gz"
       sha256 "PLACEHOLDER_X86_SHA256"
     end
   end
